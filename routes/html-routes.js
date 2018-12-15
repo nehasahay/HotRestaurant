67745af088f917c2d4a170321185ffb644144c5d
N <username@users.noreply.github.com>
2018-12-15 11:21:56 -0500

    typo

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,15 @@ const path = require('path');
 
 module.exports = app => {
     app.get('/tables', (request, response) => {
-        res.sendFile(path.join(__dirname + '/../public/tables.html'));
+        response.sendFile(path.join(__dirname + '/../public/tables.html'));
     })
 
     app.get('/reserve', (request, response) => {
-        res.sendFile(path.join(__dirname + '/../public/reserve.html'));
+        response.sendFile(path.join(__dirname + '/../public/reserve.html'));
     });
 
     // If no matching route is found default to home
     app.use((request, response) => {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        response.sendFile(path.join(__dirname + '/../public/home.html'));
     });
 }
