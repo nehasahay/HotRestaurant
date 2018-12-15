0a7d00f1256b6cdb5433463531ece474f919b44d
N <username@users.noreply.github.com>
2018-12-15 14:35:20 -0500

    delete button

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,36 +3,36 @@
 var tableData = require("../data/tableData");
 var waitListData = require("../data/waitinglistData");
 
-module.exports = function(app) {
-  app.get("/api/tables", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/tables", function (req, res) {
     res.json(tableData);
   });
 
-  app.get("/api/waitlist", function(req, res) {
+  app.get("/api/waitlist", function (req, res) {
     res.json(waitListData);
   });
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/tables", function (req, res) {
     if (tableData.length < 5) {
       tableData.push(req.body);
       res.json(true);
-    }
-   
-    else {
+    } else {
       waitListData.push(req.body);
       res.json(false);
     }
   });
 
-  app.delete("/api/tables/:customer", function(req, res) {
+  app.delete("/api/tables/:customer", function (req, res) {
     var customer = req.params.customer;
     tableData.splice(customer, 1);
-    var newCustomer = waitListData.pop();
-    tableData.push(newCustomer);
+    if (waitListData.length) {
+      var newCustomer = waitListData.shift();
+      tableData.push(newCustomer);
+    };
     res.json(true);
   });
 
-  app.post("/api/clear", function() {
+  app.post("/api/clear", function () {
     tableData = [];
     waitListData = [];
 
